6b8400c1948487158ebe1a573475a750b39f3d4a
Kenny Li <kennyli138@gmail.com>
2019-08-15 21:41:27 -0500

    Added nav component

--- app/src/main/java/com/example/money_machine/MainActivity.kt
+++ app/src/main/java/com/example/money_machine/MainActivity.kt
@@ -3,28 +3,41 @@ package com.example.money_machine
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.nav_spending -> {
-                message.setText(R.string.tab_spending)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.nav_saving -> {
-                message.setText(R.string.tab_saving)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.nav_spending -> {
+//                message.setText(R.string.tab_spending)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.nav_saving -> {
+//                message.setText(R.string.tab_saving)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+//        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
 
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    private fun setupNavigation() {
+        val navController = Navigation.findNavController(this, R.id.nav_fragment)
+        setupActionBarWithNavController(navController)
+        NavigationUI.setupWithNavController(bottom_navigation, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_fragment).navigateUp()
     }
 }
