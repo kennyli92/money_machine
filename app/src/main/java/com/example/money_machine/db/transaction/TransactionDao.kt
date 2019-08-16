032eba7b3dbab79340315603483350235275a96d
Kenny Li <kennyli138@gmail.com>
2019-08-16 03:27:33 -0500

    Updated Transaction Dao

--- app/src/main/java/com/example/money_machine/db/transaction/TransactionDao.kt
+++ app/src/main/java/com/example/money_machine/db/transaction/TransactionDao.kt
@@ -2,9 +2,18 @@ package com.example.money_machine.db.transaction
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.reactivex.Completable
+import io.reactivex.Flowable
 
 @Dao
 interface TransactionDao {
-  @Insert
-  fun insert(transaction: Transaction)
+  // create new transaction record. If record already exist, replace it
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insert(transaction: Transaction): Completable
+
+  // get all transaction by type: spending or saving
+  @Query("SELECT * FROM `transaction` WHERE isSpending = :isSpending")
+  fun getTransactionsByType(isSpending: Boolean): Flowable<Transaction>
 }
